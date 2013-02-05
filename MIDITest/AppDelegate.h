d8ab8dc60a83b9fd64a3175c041f634046277ea5
David Drechsel <daviddrechsel@Davids-MacBook-Air.local>
2013-02-05 08:36:45 +0100

    Initial Commit

---
+++ MIDITest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MIDITest
+//
+//  Created by David Drechsel on 05.02.13.
+//  Copyright (c) 2013 David Drechsel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MIDITest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MIDITest
+//
+//  Created by David Drechsel on 05.02.13.
+//  Copyright (c) 2013 David Drechsel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
